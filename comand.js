f7a16b045b60f6147c73bf39a8afe1a3bf089cc1
Max-Rus1977 <maxsvet77@yandex.ru>
2021-08-15 07:30:38 +0300

    loading in github

--- comand.js
+++ comand.js
@@ -1,16 +1,50 @@
 const bottons = document.querySelectorAll('.btn');
-const modal = document.querySelector('.bacground__box-images');
+const genContainer = document.querySelector('.content-wrapper');
+
+let texBtn = '';
 
 for (let botton of bottons) {
-  botton.addEventListener('click', clickBtn)
+  botton.addEventListener('click', function clickBtn() {
+    texBtn = botton.textContent;
+    createModalImg();
+  })
 }
 
-function clickBtn() {
-  modal.classList.add('activ-js');
+function createModalImg() {
+  const bacgroundModalTeg = document.createElement('div');
+  const boxImagesTeg = document.createElement('div');
+  const imgTeg = document.createElement('img');
+
+  bacgroundModalTeg.classList.add('bacground__box-images');
+  boxImagesTeg.classList.add('box-images');
+  console.log(texBtn);
+  imgTeg.src = imagesSrc[texBtn];
+  console.log(imagesSrc[texBtn]);
+
+
+  bacgroundModalTeg.appendChild(boxImagesTeg);
+  boxImagesTeg.appendChild(imgTeg);
+  genContainer.appendChild(bacgroundModalTeg);
+
+  bacgroundModalTeg.classList.add('activ-js');
+  setTimeout(ad, 2000);
+
+  function ad() {
+    bacgroundModalTeg.classList.add('activ-js');
+  }
+
 }
 
-modal.addEventListener('click', cloceModal);
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('bacground__box-images')) {
+    event.target.remove();
+  }
+})
 
-function cloceModal() {
-  modal.classList.remove('activ-js');
+const imagesSrc = {
+  cart: "img/basket.png",
+  fire: "img/campfire.png",
+  confetti: "img/confetti.png",
+  paper: "img/paper-crafts.png"
 }
+
